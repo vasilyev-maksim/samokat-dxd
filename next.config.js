2ee6fad5f9b7cc8c4324ad070343fbdfdf8fdbda
Maksim Vasilyev <v.maksim.vasilyev@reddit.com>
2024-04-11 18:56:59 +0400

    upd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.SERVER === "production";
 
 module.exports = {
   output: "export",
