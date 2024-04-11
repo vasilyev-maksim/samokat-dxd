f730a4d594d275e7fe0a31adf8f25fbabf1c9566
Maksim Vasilyev <v.maksim.vasilyev@reddit.com>
2024-04-11 16:13:24 +0400

    Opengraph fix

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,6 @@
 export function url(str: string) {
   return (
-    (process.env.SERVER === "production"
+    (process.env.NODE_ENV === "production"
       ? `https://f00852f9-9419-4a17-9a7c-cb5d30cbb45f.selcdn.net`
       : "") + str.replace(/^\./, "")
   );
