c4983f259ef20715d541e59a1188b040c79abe73
Maksim Vasilyev <v.maksim.vasilyev@reddit.com>
2024-04-11 18:56:59 +0400

    upd

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,6 @@
 export function url(str: string) {
   return (
-    (process.env.NODE_ENV === "production"
+    (process.env.SERVER === "production"
       ? `https://f00852f9-9419-4a17-9a7c-cb5d30cbb45f.selcdn.net`
       : "") + str.replace(/^\./, "")
   );
