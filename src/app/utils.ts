b3e788dd45dc82365bd2e0f37eabbdca95b13d04
Maksim Vasilyev <v.maksim.vasilyev@reddit.com>
2024-04-11 15:59:03 +0400

    SERVER env var for prod

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,9 @@
 export function url(str: string) {
-  return str;
+  // return str;
+  console.log(process.env.SERVER);
   return (
-    `https://f00852f9-9419-4a17-9a7c-cb5d30cbb45f.selcdn.net` +
-    str.replace(/^\./, "")
+    (process.env.SERVER === "production"
+      ? `https://f00852f9-9419-4a17-9a7c-cb5d30cbb45f.selcdn.net`
+      : "") + str.replace(/^\./, "")
   );
 }
