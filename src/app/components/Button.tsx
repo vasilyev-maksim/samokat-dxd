0928cc2dbd04cb4acc2158480b63167e122e00f2
Maksim Vasilyev <v.maksim.vasilyev@reddit.com>
2024-04-12 15:57:11 +0400

    slider items made links

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import config from "../config.json";
 
 export const Button: React.FC<{
   label: string;
@@ -11,9 +12,7 @@ export const Button: React.FC<{
     <div className={"link-button-wrapper " + className} style={{ marginTop }}>
       <a
         target={target}
-        href={
-          "https://vml8.adj.st/promocategory/9da5ea84-b591-4f47-9287-61fb77f8632a?showcaseType=MINIMARKET&adj_t=10oehtvh&adj_campaign=childexpert_samokat&adj_adgroup=podborka_products&adj_fallback=https%3A%2F%2Fsamokat.ru%2Fpromo%2F9da5ea84-b591-4f47-9287-61fb77f8632a%3Futm_source%3Dpromoaction_marketing%26utm_medium%3Dchildexpert_smkt%26utm_campaign%3Dapr24_podborka&adj_redirect_macos=https%3A%2F%2Fsamokat.ru%2Fpromo%2F9da5ea84-b591-4f47-9287-61fb77f8632a%3Futm_source%3Dpromoaction_marketing%26utm_medium%3Dchildexpert_smkt%26utm_campaign%3Dapr24_podborka"
-        }
+        href={config.appLink}
         className="link-button"
         onClick={onClick}
       >
