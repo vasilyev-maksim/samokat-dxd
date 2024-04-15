36e7d5dd8f5348b677af954f786c30af79e45fbf
Maksim Vasilyev <v.maksim.vasilyev@reddit.com>
2024-04-15 21:26:39 +0400

    Footer link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,11 @@ export default function Home() {
             <a target="_blank" href="https://groznov.co/">
               GROZNOV
             </a>
-            ®
+            ® &nbsp; &nbsp; &nbsp;{" "}
+            <a target="_blank" href="https://cloud.mail.ru/public/uvKj/6vnCBkMom/%D0%91%D1%8D%D0%BA%D1%81%D1%82%D0%B5%D0%B9%D0%B4%D0%B6">
+              Фото с бекстейджа
+            </a>{" "}
+            ♡
           </div>
         </footer>
         <div className="bottom-sticked-block">
